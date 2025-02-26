e0baa29e0cf8f395e7e044fcce2ea7aa89d3fec9
pablo-mu <pablochulilla15@gmail.com>
2025-02-26 11:23:19 +0100

    feat: MixMatch components

--- utils/mixmatch.py
+++ utils/mixmatch.py
@@ -2,7 +2,61 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
+
+def interleave_offsets(batch, nu):
+    """Calculate the inidices to split a batch into several parts that are as balanced as possible.
+
+    Args:
+        batch (int): The batch size.
+        nu (_type_): Number of parts to split the batch into.
+    """
+    groups = [batch // (nu + 1)] * (nu + 1)
+    for x in range(batch - sum(groups)):
+        groups[-x - 1] += 1
+    offsets = [0]
+    for g in groups:
+        offsets.append(offsets[-1] + g)
+    assert offsets[-1] == batch
+    return offsets
+
+def interleave(xy, batch):
+    """Interleave labeled and unlabeled samples.
+
+    Args:
+        xy ( list of torch.Tensor): A list of tensors to interleave.
+        batch (int): The batch size.
+
+    Returns:
+        list of torch.Tensor: The interleaved tensor.
+    """
+    nu = len(xy) - 1
+    offsets = interleave_offsets(batch, nu)
+    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]
+    for i in range(1, nu + 1):
+        xy[0][i], xy[i][i] = xy[i][i], xy[0][i]
+    return [torch.cat(v, dim=0) for v in xy]
+  
+    
+
+
 def sharpen(prob, T):
     p_power = prob ** (1 / T)
     return p_power / p_power.sum(dim=1, keepdim=True)
 
+def linear_rumpup(current, rampup_length):
+    if rampup_length == 0:
+        return 1.0
+    else:
+        current = np.clip(current / rampup_length, 0.0, 1.0)
+        return float(current)
+        
+
+class SemiLoss(object):
+    def __call__(self, outputs_x, targets_x, outputs_u, targets_u, epoch, epochs):
+        probs_u = torch.softmax(outputs_u, dim = 1)
+        
+        # Supervised and unsupervised loss
+        Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
+        Lu = torch.mean((probs_u - targets_u)**2)
+
+        return Lx, Lu, linear_rumpup(epoch, epochs)
